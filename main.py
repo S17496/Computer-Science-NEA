67a76dd7980d41d52cad19615e5d9a8e64fee942
S17496 <s17496@qmc.ac.uk>
2026-05-22 10:31:52 +0100

    Added player class.

--- main.py
+++ main.py
@@ -9,9 +9,20 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.Surface((40, 60))
+        self.image.fill((255, 200, 0))
+        self.rect = self.image.get_rect(topleft=(x, y))
+    
+
+player1 = Player(500, 500)
+player1_group = pygame.sprite.Group(player1)
+
 running = True
 while running:
     screen.fill((0,0,150))
-    
+    player1_group.draw(screen)
     # 60 FPS
     clock.tick(60)
