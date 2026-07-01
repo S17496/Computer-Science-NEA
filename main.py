14f9ef7f2a3d00fc26b493bf368b4b7a0707a20d
S17496 <s17496@qmc.ac.uk>
2026-07-01 15:21:24 +0100

    Update main.py
    Added a world made up of tiles.

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import pygame
 # Constants
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
+TILE_SIZE = 128
+
 
 # Pygame Initialisations
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+
+world = [[0,0,0,0,0,0,0,0,0,0],[0,1,1,1,0,0,0,1,1,0],[0,1,0,1,0,0,0,1,0,0],[1,1,1,1,1,1,1,1,1,1]]
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -51,22 +55,44 @@ class Player(pygame.sprite.Sprite):
             self.on_ground = True
 
 
-
-    
-
 player1 = Player(500, 500)
-player1_group = pygame.sprite.Group(player1)
+
+tiles = []
+for row_index, row in enumerate(world):
+    for col_index, tile in enumerate(row):
+        if tile == 1:
+            rect = pygame.Rect(col_index * TILE_SIZE, row_index * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+            tiles.append(rect)
+
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    
+    player1.update()
+
+
+    camera_x = player1.rect.centerx - SCREEN_WIDTH//2
+    camera_y = player1.rect.centery - SCREEN_HEIGHT//2
+
     screen.fill((0,0,150))
 
-    player1_group.update()
-    player1_group.draw(screen)
+    for tile in tiles:
+            pygame.draw.rect(screen,(100,200,100),(tile.x - camera_x,tile.y - camera_y, TILE_SIZE, TILE_SIZE))
+    
+    
 
+
+
+
+
+
+    screen.blit(player1.image, (player1.rect.x - camera_x, player1.rect.y - camera_y))
+   
     pygame.display.update()
     # 60 FPS
     clock.tick(60)
