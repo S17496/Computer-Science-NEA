be4701da43b9d5c9d885007ab77e7f9d522fd651
S17496 <s17496@qmc.ac.uk>
2026-07-07 13:20:15 +0100

    added a function to world which finds tiles in a 5x5 near the player

--- world_file.py
+++ world_file.py
@@ -5,8 +5,8 @@ import tile_file as t
 class World:
     # Constructor
     def __init__(self, world_data):
-        self.tile_group = pygame.sprite.Group()
-        self.tile_dic = {}
+        self._tile_group = pygame.sprite.Group()
+        self._tile_dic = {}
         self.load(world_data)
     
     # Load world
@@ -16,5 +16,16 @@ class World:
             for col_index, tile in enumerate(row):
                 if tile == 1:
                     new_tile = t.Tile(col_index * c.TILE_SIZE, row_index * c.TILE_SIZE)
-                    self.tile_group.add(new_tile)
-                    self.tile_dic[(col_index, row_index)] = new_tile
+                    self._tile_group.add(new_tile)
+                    self._tile_dic[(col_index, row_index)] = new_tile
+
+    # Get nearby tiles to player
+    def get_nearby(self, rect):
+        position_x = rect.centerx // c.TILE_SIZE
+        position_y = rect.centery // c.TILE_SIZE
+        nearby = []
+        for i in range(position_x - 2, position_x + 3):
+            for j in range(position_y - 2, position_y + 3):
+                if (i,j) in self._tile_dic:
+                    nearby.append(self._tile_dic[(i,j)])
+        return nearby
